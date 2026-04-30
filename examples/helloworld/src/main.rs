340de4290804744bcf76741dad43af68f3d5aae2
yfblock <321353225@qq.com>
2026-04-30 19:34:04 +0800

    feat: update imgcat feature

--- examples/helloworld/src/main.rs
+++ examples/helloworld/src/main.rs
@@ -3,6 +3,7 @@
 
 extern crate axplat_riscv64_sg2002;
 
+mod imgcat;
 mod usb_camera;
 
 #[cfg(feature = "axstd")]
@@ -11,5 +12,27 @@ use axstd::println;
 #[cfg_attr(feature = "axstd", unsafe(no_mangle))]
 fn main() {
     println!("Hello, world!");
-    usb_camera::run();
+
+    let (cam, sel) = match usb_camera::init() {
+        Ok(v) => v,
+        Err(msg) => {
+            println!("USB/UVC init failed: {msg}");
+            loop {
+                core::hint::spin_loop();
+            }
+        }
+    };
+
+    match usb_camera::capture_frame(&cam, &sel) {
+        Ok(jpeg) => {
+            println!("imgcat: 输出 1 帧 JPEG ({} bytes)", jpeg.len());
+            imgcat::print_image(jpeg);
+            println!("imgcat: 完成");
+        }
+        Err(msg) => println!("UVC capture failed: {msg}"),
+    }
+
+    loop {
+        core::hint::spin_loop();
+    }
 }
